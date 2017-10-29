7a42b27dc551ed13f52e11aa88cc4122b6a06fee
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-10-29 14:02:47 -0500

    hamburger menu now fixed

--- js/splash.js
+++ js/splash.js
@@ -1,14 +1,7 @@
 'use strict';
 $( document ).ready(function() {
-
-    if($(".splash").is(":visible"))
-    {
-        $(".wrapper").css({"opacity":"0"});
-    }
-    $(".splash-arrow").click(function()
-    {
+    $(".splash-arrow").click(function() {
         $(".splash").slideUp("800", function() {
-              $(".wrapper").delay(100).animate({"opacity":"1.0"},800);
               $(".content").css({"display":"initial"});
          });
     });
@@ -17,7 +10,6 @@ $( document ).ready(function() {
           $(window).off("scroll");
           $(".splash").slideUp("800", function() {
             $("html, body").animate({"scrollTop":"0px"},100);
-            $(".wrapper").delay(100).animate({"opacity":"1.0"},800);
             $(".content").css({"display":"initial"});
           });
      });
