2e69510efe4e63e0cd140f478d8281c57098a496
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-10-29 16:55:47 -0500

    work on anchor links

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,11 +1,13 @@
 'use strict';
+
 $('.menu-toggle').click(() => {
 
-  $(this).toggleClass('change');
+  $('.menu-toggle').toggleClass('change');
   $('.nav-menu').toggleClass('show');
 
 });
 
 $('.nav-link').click(() => {
+	$('.menu-toggle').toggleClass('change');
 	$('.nav-menu').toggleClass('show');
 });
