f54d2f4b4a4792bf05081f805cd8291979e74692
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-10-29 13:46:01 -0500

    fixed splash functions

--- js/splash.js
+++ js/splash.js
@@ -9,14 +9,16 @@ $( document ).ready(function() {
     {
         $(".splash").slideUp("800", function() {
               $(".wrapper").delay(100).animate({"opacity":"1.0"},800);
+              $(".content").css({"display":"initial"});
          });
     });
 
     $(window).scroll(function() {
           $(window).off("scroll");
           $(".splash").slideUp("800", function() {
-          $("html, body").animate({"scrollTop":"0px"},100);
-          $(".wrapper").delay(100).animate({"opacity":"1.0"},800);
-        });
+            $("html, body").animate({"scrollTop":"0px"},100);
+            $(".wrapper").delay(100).animate({"opacity":"1.0"},800);
+            $(".content").css({"display":"initial"});
+          });
      });
 });
