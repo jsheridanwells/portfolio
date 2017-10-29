910bb12953cd5fb8bbc55f477ba0c753337880b9
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-10-29 17:59:46 -0500

    fixed scrolling splash problem

--- js/splash.js
+++ js/splash.js
@@ -1,16 +1,22 @@
 'use strict';
+//something
 $( document ).ready(function() {
-    $(".splash-arrow").click(function() {
-        $(".splash").slideUp("800", function() {
-              $(".content").css({"display":"initial"});
-         });
-    });
-
-    $(window).scroll(function() {
-          $(window).off("scroll");
-          $(".splash").slideUp("800", function() {
-            $("html, body").animate({"scrollTop":"0px"},100);
-            $(".content").css({"display":"initial"});
-          });
+  if($(".splash").is(":visible"))
+  {
+    $(".wrapper").css({"opacity":"0"});
+  }
+  $(".splash-arrow").click(function()
+  {
+    $(".splash").slideUp("800", function() {
+        $(".wrapper").delay(100).animate({"opacity":"1.0"},800);
      });
+  });
+});
+
+$(window).scroll(function() {
+      $(window).off("scroll");
+    $(".splash").slideUp("800", function() {
+    $("html, body").animate({"scrollTop":"0px"},100);
+    $(".wrapper").delay(100).animate({"opacity":"1.0"},800);
+ });
 });
