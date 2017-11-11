30ef01ec1094da8dbdc5fb743c23fc50f80ca986
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-11-11 15:12:57 -0600

    js is working

---
+++ js/nav-highlighting.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var navHighlighter = function(){
+
+  window.addEventListener('scroll', () => {
+
+    let buffer = 10;
+
+    let aboutTop = $('#about').offset().top - buffer;
+    let techTop = $('#tech').offset().top - buffer;
+    let projectsTop = $('#projects').offset().top - buffer;
+    let contactTop = $('#contact').offset().top - buffer;
+    let top = $(window).scrollTop();
+
+    if (top >= aboutTop && top <= techTop) {
+      $('li').removeClass('active');
+      $('#about-nav').addClass('active');
+    } else if (top >= techTop && top <= projectsTop) {
+      $('li').removeClass('active');
+      $('#tech-nav').addClass('active');
+    } else if (top >= projectsTop && top <= contactTop) {
+      $('li').removeClass('active');
+      $('#projects-nav').addClass('active');
+    } else if (top >= contactTop) {
+      $('li').removeClass('active');
+      $('#contact-nav').addClass('active');
+    }
+
+    console.log(top, aboutTop);
+
+  });
+
+}();
