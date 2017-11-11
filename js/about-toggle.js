28e03b7ce9f65a9fae5414999b4dfb66502480ed
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-11-11 11:42:13 -0600

    shows more 'about' on click

--- js/about-toggle.js
+++ js/about-toggle.js
@@ -4,3 +4,8 @@ $('.toggle-more-sm').click(() => {
   $('.about-more').slideDown();
   $('.toggle-more-sm').css({'display' : 'none'});
 });
+
+$('.toggle-more-lg').click(() => {
+  $('.about-more').slideDown();
+  $('.toggle-more-lg').css({'display' : 'none'});
+});
